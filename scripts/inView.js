412908676deafd20424306a67dacd5d9c879363d
kate <kate_evstratenko@mail.ru>
2014-07-15 02:55:49 +0300

    added pseudo-selector 'inView'

---
+++ scripts/inView.js
@@ -0,0 +1,32 @@
+﻿(function () {
+    $.extend($.expr.pseudos, {
+        inView: function (e) {
+
+            var viewTop = $(window).scrollTop()//координата начала видимой области
+            var viewBottom = viewTop + $(window).height();//конец видимой области
+            var viewLeft = $(window).scrollLeft()
+            var viewRight = viewLeft + $(window).width();
+
+            console.log(viewTop);
+            console.log(viewBottom);
+
+            var elTop = $(e).offset().top;
+            var elBottom = elTop + $(e).outerHeight();
+            var elLeft = $(e).offset().left;
+            var elRight = elLeft + $(e).outerWidth();
+
+            console.log(e);
+            console.log(elTop)
+            console.log(elBottom + '\n');
+
+            if (elTop >= viewTop  && elBottom <= viewBottom &&
+                elLeft >= viewLeft && elRight <= viewRight) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    });
+}());
+
---
+++ scripts/program.js
@@ -0,0 +1,6 @@
+﻿var inViewElements = $('.mainContent .section:inView');
+
+console.log('count: ' + inViewElements.length);
+for (var i = 0; i < inViewElements.length; i++) {
+    console.log(inViewElements[i]);
+}
