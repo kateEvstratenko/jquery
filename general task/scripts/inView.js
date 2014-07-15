dd7162eb764dd35b141dc696492b19882dbcdddb
kate <kate_evstratenko@mail.ru>
2014-07-15 18:12:48 +0300

    changed inView.js: style of code is improved

---
+++ general task/scripts/inView.js
@@ -0,0 +1,21 @@
+﻿(function () {
+    $.extend($.expr.pseudos, {
+        inView: function (e) {
+
+            var viewTop = $(window).scrollTop();
+            var viewBottom = viewTop + $(window).height();
+            var viewLeft = $(window).scrollLeft();
+            var viewRight = viewLeft + $(window).width();
+
+            var elTop = $(e).offset().top;
+            var elBottom = elTop + $(e).outerHeight();
+            var elLeft = $(e).offset().left;
+            var elRight = elLeft + $(e).outerWidth();
+
+            var isInView = (elTop >= viewTop && elBottom <= viewBottom &&
+                            elLeft >= viewLeft && elRight <= viewRight);
+            return isInView;
+        }
+    });
+}());
+
--- scripts/program.js
+++
@@ -1,6 +0,0 @@
-﻿var inViewElements = $('.mainContent .section:inView');
-
-console.log('count: ' + inViewElements.length);
-for (var i = 0; i < inViewElements.length; i++) {
-    console.log(inViewElements[i]);
-}
